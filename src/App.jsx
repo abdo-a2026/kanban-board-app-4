eaaf47772c919a33dc877aa24ef69f3c85eaccf3
Abdullah <bwdy72937@gmail.com>
2026-06-19 15:34:19 +0000

    optimiz files structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,28 @@ import { useSelector } from "react-redux";
 // import viteLogo from "/vite.svg";
 import "./App.css";
 
-import Header from "./combonents/header";
-import Box from "./combonents/box";
-import NewTaskForm from "./combonents/newTaskForm";
-import UpdateTaskForm from "./combonents/updateTaskForm";
+import Header from "./combonents/layout/header";
+import Box from "./features/kanban-board/combonents/box";
+import NewTaskForm from "./features/kanban-board/combonents/newTaskForm";
+import UpdateTaskForm from "./features/kanban-board/combonents/updateTaskForm";
+import Toast from "./combonents/ui/toast";
+import ConfirmDialog from "./combonents/ui/confirmDialog";
 
 function App() {
   // const [count, setCount] = useState(0);
   const [isModalOpen, setIsModalOpen] = useState(false);
   // const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(true);
-  const {currentTask} = useSelector((state) => state.tasks)
-
+  const { currentTask } = useSelector((state) => state.tasks);
 
   return (
     <>
       <div className="min-h-screen flex flex-col font-sans w-full">
         <Header setIsModalOpen={setIsModalOpen} />
         <Box />
+        <Toast />
+        <ConfirmDialog />
         {isModalOpen && <NewTaskForm setIsModalOpen={setIsModalOpen} />}
-        {currentTask && (
-          <UpdateTaskForm />
-        )}
+        {currentTask && <UpdateTaskForm />}
       </div>
     </>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import tasks from "./taskSlise";
+import tasks from "../features/kanban-board/services/taskSlice";
+import notics from "./uiSlice";
 
 const store = configureStore({
-  reducer: { tasks: tasks },
+  reducer: { tasks: tasks, notics: notics },
 });
 
 export default store;
